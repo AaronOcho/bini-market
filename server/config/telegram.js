1037ef21e6ba1e80120c1d600ce8428842f638c3
AaronOcho <aaronbot2345@gmail.com>
2025-05-07 22:08:09 +0800

    Update telegram.js

--- server/config/telegram.js
+++ server/config/telegram.js
@@ -1,11 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot('7762890246:AAH0gl9zd4bTe8u43zhVtihYiuYQYkAtsf4', { polling: false });
-const ADMIN_TELEGRAM_ID = '7057793599';
-const ADMIN_TELEGRAM_USERNAME = '@coletsbini';
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+const ADMIN_TELEGRAM_ID = process.env.TELEGRAM_ADMIN_ID;
 
 module.exports = {
     bot,
-    ADMIN_TELEGRAM_ID,
-    ADMIN_TELEGRAM_USERNAME
+    ADMIN_TELEGRAM_ID
 };
