8adc44cd01d8b9ed4d8b217a9b7b70e19368fe2d
AaronOcho <aaronbot2345@gmail.com>
2025-05-07 21:55:25 +0800

    Create config.js

---
+++ client/public/js/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    ADMIN_EMAIL: 'coletbini2025@gmail.com',
+    ADMIN_PASSWORD: 'coletbini2025',
+    GCASH_NUMBER: '09764595236',
+    BINANCE_ID: '1095880042',
+    TELEGRAM_USERNAME: '@coletsbini',
+    API_BASE_URL: '/api'
+};
+
+const PAYMENT_METHODS = {
+    GCASH: 'GCash',
+    BINANCE: 'Binance'
+};
