045278aaaa53049186b89658b19a8f79443fc199
AaronOcho <aaronbot2345@gmail.com>
2025-05-07 22:07:47 +0800

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgresql://Ocho%20Database_owner:npg_5BDhT1UzSWRA@ep-morning-frog-a10qopdc-pooler.ap-southeast-1.aws.neon.tech/Ocho%20Database?sslmode=require'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
